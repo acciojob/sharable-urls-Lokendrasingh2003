9092bf1fe3fa65bb601bb69ced89d7d048c98a31
Lokendra Singh <136624583+Lokendrasingh2003@users.noreply.github.com>
2026-05-27 13:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+const form=document.getElementById("form")
+const nameInput=document.getElementById("name")
+const yearInput=document.getElementById("year")
+const url=document.getElementById("url")
+form.addEventListener("submit",function(e){
+   e.preventDefault()
+	let baseUrl="https://localhost:8080/"
+	let params=[]
+	if(nameInput.value.trim()!==""){
+		params.push(`name=${nameInput.value}`)
+		
+	}
+	if(yearInput.value.trim()!==""){
+		params,push(`year=${yearInput.value}`)
+	}
+	if(params.length>0){
+		baseUrl += "?" + params.join("&")
+	}
+
+	url.textContent=baseUrl
+	   
+	
+})
